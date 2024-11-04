3369dfaa3836e50cfa1ea789ef152d9994417dda
GrovesJ <j.groves@student.rug.nl>
2024-11-04 15:54:50 +0100

    Implemented feature class

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -6,7 +6,13 @@ import numpy as np
 from autoop.core.ml.dataset import Dataset
 
 class Feature(BaseModel):
-    # attributes here
+    """
+    Feature class takes a name and type of a feature
+    """
+    name:  str = Field(..., description="The name of the feature")
+    feature_type: Literal['numeric', 'categorical'] = Field(..., description="The type of feature")
+
 
     def __str__(self):
-        raise NotImplementedError("To be implemented.")
+        return f"Feature(name={self.name}, type={self.feature_type})"
+    
