e656c8afdfc9647aedf67dafc800e24fbd545a9a
GrovesJ <j.groves@student.rug.nl>
2024-11-04 15:00:31 +0100

    Features First Push

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -2,6 +2,8 @@
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
+import pandas
+from pandas.api.types import is_numeric_dtype
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
@@ -10,4 +12,12 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: List of features with their types.
     """
+    data_frame = pandas.read_csv('dataset.csv')
+
+    for column in data_frame.columns:
+        if data_frame[column].dtype.name == 'int64':
+            print(f"{column} is numeric")
+        elif data_frame[column].dtype.name == 'category':
+            print(f"{column} is categorical")
+
     raise NotImplementedError("This should be implemented by you.")
