2205f28b28a16919dd2bd54af69fb465e7552afb
GrovesJ <j.groves@student.rug.nl>
2024-11-10 22:30:21 +0100

    flake8 type hints

--- autoop/core/ml/model/regression/support_vector_regressor.py
+++ autoop/core/ml/model/regression/support_vector_regressor.py
@@ -1,5 +1,6 @@
 from sklearn.svm import SVR
 from autoop.core.ml.model import Model
+import pandas as pd
 
 
 class SupportVectorRegressor(Model):
@@ -7,19 +8,20 @@ class SupportVectorRegressor(Model):
     Support Vector Machine model for regression tasks.
     """
     def __init__(self):
+        """ Initialize Support Vector Regressions """
         super().__init__("regression")
         self.hyperparameters["C"] = 1.0
         self.hyperparameters["degree"] = 3
         self.model = SVR()
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame) -> None:
         """
         Fit the model to the training data.
         """
         self.model.fit(X, y)
         self.parameters["support_vectors"] = self.model.support_
 
-    def predict(self, X):
+    def predict(self, X: pd.DataFrame) -> pd.DataFrame:
         """
         Make predictions on the test data.
         """
