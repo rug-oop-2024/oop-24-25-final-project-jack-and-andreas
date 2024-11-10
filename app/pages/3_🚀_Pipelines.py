a1badcb8fe345a0f8628e28d95b30de9e6eee262
AChitos <104570632+AChitos@users.noreply.github.com>
2024-11-10 15:10:38 +0100

    loading part almost works

--- app/pages/3_🚀_Pipelines.py
+++ app/pages/3_🚀_Pipelines.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 from app.core.system import AutoMLSystem
-
+import io
 
 st.set_page_config(page_title="Pipelines", page_icon="🚀")
 
@@ -11,9 +12,17 @@ def write_helper_text(text: str):
 
 st.write("# 🚀 Pipelines")
 
-
 automl = AutoMLSystem.get_instance()
 
 pipelines = automl.registry.list("pipeline_config")
 pipeline = st.selectbox("Select a pipeline", pipelines,
                         format_func=lambda x: f"{x.name} - {x.version}")
+
+if st.button("Load Datasets"):
+    if pipeline:
+        st.write(f"### Dataset: {pipeline.name} (version {pipeline.version})")
+        data_bytes = pipeline.read()
+        dataframe = pd.read_csv(io.BytesIO(data_bytes), encoding='ISO-8859-1')
+        st.dataframe(dataframe.head(10))
+    else:
+        st.write("No datasets selected.")
