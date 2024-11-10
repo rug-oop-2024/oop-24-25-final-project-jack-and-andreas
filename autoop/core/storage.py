85f5690c31ba97793f7ec8015734d5be4432266f
AChitos <104570632+AChitos@users.noreply.github.com>
2024-11-10 20:36:38 +0100

    flake 8

--- autoop/core/storage.py
+++ autoop/core/storage.py
@@ -5,13 +5,7 @@ from glob import glob
 
 
 class NotFoundError(Exception):
-    """
-    Error for when a path is not found
-    """
     def __init__(self, path):
-        """
-        Initialize the error
-        """
         super().__init__(f"Path not found: {path}")
 
 
@@ -59,18 +53,12 @@ class Storage(ABC):
 
 
 class LocalStorage(Storage):
-    """
-    Local storage implementation
-    """
     def __init__(self, base_path: str = "./assets"):
         self._base_path = base_path
         if not os.path.exists(self._base_path):
             os.makedirs(self._base_path)
 
     def save(self, data: bytes, key: str):
-        """
-        Save data to a given path
-        """
         path = self._join_path(key)
         if not os.path.exists(path):
             os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -78,40 +66,25 @@ class LocalStorage(Storage):
             f.write(data)
 
     def load(self, key: str) -> bytes:
-        """
-        Load data from a given path
-        """
         path = self._join_path(key)
         self._assert_path_exists(path)
         with open(path, 'rb') as f:
             return f.read()
 
     def delete(self, key: str = "/"):
-        """
-        Delete data at a given path
-        """
         self._assert_path_exists(self._join_path(key))
         path = self._join_path(key)
         os.remove(path)
 
     def list(self, prefix: str) -> List[str]:
-        """
-        List all paths under a given path
-        """
         path = self._join_path(prefix)
         self._assert_path_exists(path)
         keys = glob(path + "/**/*", recursive=True)
         return list(filter(os.path.isfile, keys))
 
     def _assert_path_exists(self, path: str):
-        """
-        Assert that a path exists
-        """
         if not os.path.exists(path):
             raise NotFoundError(path)
 
     def _join_path(self, path: str) -> str:
-        """
-        Join the base path with a given path
-        """
         return os.path.join(self._base_path, path)
